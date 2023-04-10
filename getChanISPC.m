3aef6cbdb676d2b7121dcfe8b8fc9cbe2b98d3a6
Adam Dede <30808263+adede1988@users.noreply.github.com>
2023-04-10 09:57:02 -0500

    Update getChanISPC.m

--- getChanISPC.m
+++ getChanISPC.m
@@ -8,13 +8,14 @@ function [ispc] = getChanISPC(trialDat, trialDat2, frex, numfrex, stds, srate, d
     %down sample indices to shrink the data
 %     di = [1:20:size(trialDat,1)];
 
-    ispc = zeros([length(di), numfrex, 2]); 
+    %ispc stats are: ispc raw, ppc raw, ispc z-scored, ppc z-scored
+    ispc = zeros([length(di), numfrex, 4]); 
 
     padDat = mirrorPad(trialDat(:,trials)); 
     padDat2 = mirrorPad(trialDat2(:,trials)); 
 
    
-
+    bootSamps = 1000; 
     hz = linspace(0, srate, size(padDat,1) );
 
     fftDat = fft(padDat); 
@@ -41,20 +42,54 @@ function [ispc] = getChanISPC(trialDat, trialDat2, frex, numfrex, stds, srate, d
         fDat = fDat(di,:); 
         fDat2 = fDat2(di,:); 
 
+
+        %samples X time X stats (ISPC, PPC)
+        bootDist = zeros(bootSamps, length(di), 2); 
+
+        for bb = 1:bootSamps
+            
+            offSet = randsample(2:size(fDat,1), 1);
+            temp = zeros(size(fDat)); 
+            temp(offSet+1:end,:) = fDat(1:size(fDat,1)-offSet,:);
+            temp(1:offSet,:) = fDat(size(fDat,1)-offSet+1:end,:); 
+            
+            bootDist(bb,:,1) = arrayfun(@(x) abs(sum(exp(1i * (temp(x,:) - fDat2(x,:))))./size(temp,2)) , 1:size(temp,1) );
+
+            difs = temp - fDat2; 
+            N = size(difs,2); 
+            bootDist(bb,:,2) = mean(...
+                cell2mat(arrayfun(@(j) ...
+                    cell2mat(arrayfun(@(k) ...
+                        cos(difs(:,j)).*cos(difs(:,k)) + sin(difs(:,j)).*sin(difs(:,k)), ...
+                    j+1:N, 'uniformoutput', false)), ...
+                1:N-1, 'uniformoutput', false)),...
+            2);
+
+
+        end
+
         %get ispc
         ispc(:,fi,1) = arrayfun(@(x) abs(sum(exp(1i * (fDat(x,:) - fDat2(x,:))))./size(fDat,2)) , 1:size(fDat,1) );
-        
+        ispcMean = mean(bootDist(:,:,1), [1,2]);  
+        ispcSD = std(bootDist(:,:,1), [],[1,2]); 
+        ispc(:,fi,3) = (ispc(:,fi,1) - ispcMean) / ispcSD; 
+
+
+
         %implementing pairwise phase consistency from Vink et al., 2010 
+        difs = fDat - fDat2; 
+        %equation 14
+        ispc(:,fi,2) = mean(...
+                    cell2mat(arrayfun(@(j) ...
+                        cell2mat(arrayfun(@(k) ...
+                            cos(difs(:,j)).*cos(difs(:,k)) + sin(difs(:,j)).*sin(difs(:,k)), ...
+                        j+1:N, 'uniformoutput', false)), ...
+                    1:N-1, 'uniformoutput', false)),...
+                2);
 
-        %equation 15: but taking the looping parameters from equation 14
-        difVals = cell2mat(arrayfun(@(j) ...
-                    cell2mat(arrayfun(@(k) ...
-                        cos(fDat(:,j)).*cos(fDat2(:,k)) + sin(fDat(:,j)).*sin(fDat2(:,k)), j+1:size(fDat,2), ...
-                    'uniformoutput', false)),...
-                  1:size(fDat,2)-1, 'uniformoutput', false));
-
-        %equation 14 using equation 15 as input: 
-        ispc(:,fi,2) = arrayfun(@(tt) (2/(size(fDat,2)*(size(fDat,2)-1))) * sum(difVals(tt,:)), 1:size(difVals,1) ); 
+        ppcMean = mean(bootDist(:,:,2), [1,2]);  
+        ppcSD = std(bootDist(:,:,2), [],[1,2]); 
+        ispc(:,fi,4) = (ispc(:,fi,2) - ppcMean) / ppcSD; 
        
 
 
