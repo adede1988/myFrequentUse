70946f0b6184f6974be9b455616d3489b50df441
Adam Dede <30808263+adede1988@users.noreply.github.com>
2023-05-08 15:57:37 -0500

    Update myChanZscore.m

--- myChanZscore.m
+++ myChanZscore.m
@@ -1,18 +1,40 @@
-function zDat = myChanZscore(data)
+function zDat = myChanZscore(varargin) 
+   
 %data:              time X trials
+%basePeriod:        [start, stop] indices from which to draw the baseline
 
 
 %zDat:              trials X time 
 
 
+
+
+switch nargin
+    case 1
+        data = varargin{1}; 
+        basePeriod = [1, size(data,1)];
+        
+    case 2
+        data = varargin{1}; 
+        basePeriod = varargin{2}; 
+    otherwise
+        warning('Error: at least one input is needed')
+        return
+end
+
+
+
+
+
+
 nsamps = 1000; 
 
-%permute it to channels X time X trials
-% data = permute(data, [2,3,1]); 
+
 ntrials = size(data,2); 
 
 
-tmp = data(:); 
+tmp = data(basePeriod(1):basePeriod(2), :);
+tmp = tmp(:); 
 
 dist = zeros(nsamps,1); 
 for ii = 1:nsamps
@@ -30,3 +52,9 @@ zDat = (data - mean(dist)) ./ std(dist);
 
 
 end
+
+
+
+
+%permute it to channels X time X trials
+% data = permute(data, [2,3,1]); 
